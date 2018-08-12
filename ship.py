6c6da37a551baa7804c25edabaf2fc0f97091e9d
AaronLaw <aaronlaw@gmail.com>
2018-08-12 22:56:43 +0800

    Allowing continuous movement.

--- ship.py
+++ ship.py
@@ -13,6 +13,15 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # Movement flag
+            # When the player holds down the right arrow key, we want the ship to continue moving right until the player releases the key. We’ll have our game detect a pygame.KEYUP event so we’ll know when the right arrow key is released; then we’ll use the KEYDOWN and KEYUP events together with a flag called moving_right to implement continuous motion.
+        self.moving_right = False
+
+    def update(self):
+        """Update the ships's position based on the movement flag."""
+        if self.moving_right:
+            self.rect.centerx += 1
+
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
