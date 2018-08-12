02e0c6465869b5a2792e01f5f54fbcd1394135b6
AaronLaw <aaronlaw@gmail.com>
2018-08-12 21:59:23 +0800

    Use a Settings class to store all the settings.

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ A simple RPG game to learn OOP.
 """
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
     # Initialize game and create a screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    screen.fill(ai_settings.bg_color)
     # Start the main loop for the game.
     while True:
         for event in pygame.event.get():
