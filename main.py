d4957765bd975fc8c5e44844ae34272b89a3e57f
AaronLaw <aaronlaw@gmail.com>
2018-08-12 22:33:45 +0800

    Refactoring: the game_functions module.

--- main.py
+++ main.py
@@ -21,10 +21,7 @@ def run_game():
     # Start the main loop for the game.
     while True:
         gf.check_events()
-
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 def main():
