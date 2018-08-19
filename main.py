e5e282c4ffbbd97c624fd846cd23e26045f983aa
AaronLaw <aaronlaw@gmail.com>
2018-08-20 01:43:11 +0800

    Resetting the game: reset the ship & aliens.

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def run_game():
     gf.create_fleet(ai_settings, screen, ship, aliens)
     # Start the main loop for the game.
     while True:
-        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button,aliens, ship, bullets)
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, bullets, aliens)
